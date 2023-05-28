e585495a976587789ca33cd287191375ca5b954c
Grégoire Passault <g.passault@gmail.com>
2023-05-28 16:37:54 +0200

    Amicales

---
+++ events/amicales2023.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "name" => "Amicales RSK",
+    "date_start" => "2023-06-01",
+    "date_end" => "2023-06-20",
+    "fee" => "free"
+];
--- src/Data/Events.php
+++ src/Data/Events.php
@@ -7,14 +7,21 @@ class Events
     static function getEvents(): array
     {
         $dir = __dir__ . "/../../events";
-        $events = [];
+        $events_by_date = [];
 
         foreach (scandir($dir) as $f) {
             if ($f == "." || $f == "..") continue;
             $name = explode(".", $f)[0];
+            $data = include($dir . "/" . $f);
+            $data["tag"] = $name;
 
-            $events[$name] = include($dir . "/" . $f);
-            $events[$name]["tag"] = $name;
+            $events_by_date[$data["date_start"]] = $data;
+        }
+
+        ksort($events_by_date);
+        $events = [];
+        foreach ($events_by_date as $event) {
+            $events[$event["tag"]] = $event;
         }
 
         return $events;
